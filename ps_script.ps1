22914e6c5b68a0a6faed0160b6a0f2d3ec28a2c4
Lucas Cajado <lucascajado30@gmail.com>
2025-06-03 13:38:03 -0300

    Delete ps_script.ps1

--- ps_script.ps1
+++
@@ -1,6 +0,0 @@
-Set-TimeZone -Id 'E. South America Standard Time'
-Install-PackageProvider -Name NuGet -Force
-Install-Module PSWindowsUpdate -Force -SkipPublisherCheck
-Import-Module PSWindowsUpdate
-Install-WindowsUpdate -AcceptAll -IgnoreReboot
-Install-WindowsUpdate -AcceptAll -MicrosoftUpdate -IgnoreReboot
